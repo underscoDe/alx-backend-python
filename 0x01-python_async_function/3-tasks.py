014cb10ddd25c41908a66fe9828e66a802731771
Arsene Awounou <developer.awounou@gmail.com>
2022-06-27 15:31:55 +0100

    Tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,8 @@
+"""Create a task"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Return a task"""
+    return asyncio.create_task(wait_random(max_delay))
